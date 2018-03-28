83c0b1ac4895a2253d7858d12ec214111190195e
RafaMarquesLearn <30332657+RafaMarquesLearn@users.noreply.github.com>
2018-03-28 15:10:27 -0300

    Create main.py file
    Greets the user, asks for account number and password and shows it back

---
+++ main.py
@@ -0,0 +1,8 @@
+print("****************************************")
+print("*********** Aurin Bank - ATM ***********")
+print("****************************************")
+
+account = input("Enter your account number: ")
+password = input("Enter your password: ")
+
+print("Account: {} - Password: {}".format(account, password))
