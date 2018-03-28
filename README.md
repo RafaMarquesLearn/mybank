80c19652938d6b56a97e2b7ede59b803889618bb
Rafael Marques <rafa.marques.learn@gmail.com>
2018-03-28 16:06:44 -0300

    Add accounts dict, check if account and password match, give the proper response and don't show the user account and password anymore

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MyBank
-Python learning code 1, simple ATM app
+Python learning code app 1, simple ATM app
 
 This is my first python code on a learning project to write at least 30 apps in 2018
--- main.py
+++ main.py
@@ -4,7 +4,23 @@ print("****************************************")
 print("*********** Aurin Bank - ATM ***********")
 print("****************************************")
 
-account = input("Enter your account number: ")
-password = getpass.getpass("Enter your password: ")
+user_account = input("Enter your account number: ")
+user_password = getpass.getpass("Enter your password: ")
 
-print("Account: {} - Password: {}".format(account, password))
+accounts = {
+    '0001-02': {
+        'password': 'getin', 
+        'name': 'John Doe', 
+        'active': 1
+    }, 
+    '0002-02': {
+        'password': 'iamgod', 
+        'name': 'Jane Doe', 
+        'active': 0
+    }
+}
+
+if user_account in accounts and user_password == accounts[user_account]['password']:
+    print("Welcome {}!".format(accounts[user_account]['name']))
+else:
+    print("Account or password do not match!")
