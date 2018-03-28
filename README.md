2ae8de6c033419f6897c8641247449ea2981cdd4
Rafael Marques <rafa.marques.learn@gmail.com>
2018-03-28 15:29:22 -0300

    Add function to hide user password been typed.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MyBank
 Python learning code 1, simple ATM app
 
-This is my first python code on a learning project to write at least 30 apps
+This is my first python code on a learning project to write at least 30 apps in 2018
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import getpass
+
 print("****************************************")
 print("*********** Aurin Bank - ATM ***********")
 print("****************************************")
 
 account = input("Enter your account number: ")
-password = input("Enter your password: ")
+password = getpass.getpass("Enter your password: ")
 
 print("Account: {} - Password: {}".format(account, password))
